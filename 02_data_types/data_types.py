0be84d6ce2a4748f05570cec2d70125c08f6c2f0
Sonu V Raikar <omraikar.ai@gmail.com>
2026-08-06 13:58:52 +0530

    Add Python data types examples.py

---
+++ 02_data_types/data_types.py
@@ -0,0 +1,16 @@
+# Python Data types
+
+name = "Sonu"
+age = 22
+cgpa = 8.75
+is_student = True
+phone = "6366245234"
+
+---
+
+print(type(name))
+print(type(age))
+print(type(cgpa))
+print(type(is_student))
+print(type(phone))
+
