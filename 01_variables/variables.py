8342d041d9b235985dde11f1452159e039df6232
Sonu V Raikar <omraikar.ai@gmail.com>
2026-08-06 13:51:11 +0530

    Add first Python variables program.py

---
+++ 01_variables/variables.py
@@ -0,0 +1,21 @@
+# My First Python Program
+
+name = "Sonu V Raikar"
+age = 22
+course = "BCA"
+college = "Government First Grade College"
+github = "omalien-space"
+dream = "Software Engineer"
+favorite_language = "Python"
+favorite_planet = "mars"
+
+---
+
+print("Name:", name)
+print("Age:", age)
+print("Course:", course)
+print("College:", college)
+print("GitHub:", github)
+print("Dream:", dream)
+print("Favorite Language:", favorite_language)
+print("Favorite Planet:", favorite_planet) 
