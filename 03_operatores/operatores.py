bf295e09227ee0dc042e4181e849ed1bd0b7c008
Sonu V Raikar <omraikar.ai@gmail.com>
2026-08-07 12:53:09 +0530

    Add arithmetic operatores examples.py

---
+++ 03_operatores/operatores.py
@@ -0,0 +1,32 @@
+# Python Operatores
+
+a = 10
+b = 3
+
+addition = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+remainder = a % b
+power = a ** b
+floor_division = a // b
+
+---
+
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Divition:", division)
+print("Remainder:", remainder)
+print("power:", power)
+print("Floor Division:", floor_division)
+
+---
+
+Addition       : 13
+Subtraction    :  7
+Multiplication : 30
+Division       : 3.333333333333335 
+Remainder      : 1
+Power          : 1000
+Floor Division :  3
