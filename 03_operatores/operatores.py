4e6cd43a3260ff476fa0146c766f7187e019d47f
Sonu V Raikar <omraikar.ai@gmail.com>
2026-08-07 13:44:35 +0530

    Update operatores.py

--- 03_operatores/operatores.py
+++ 03_operatores/operatores.py
@@ -49,7 +49,7 @@ print("Marks != 75:", marks != 75)
 # Logical Operator's
 
 age = 21
-has_id 
+has_id = true
 
 print("Adult and has ID:", age >= 18 and has_id == true)
 print("Under 18 or has ID:", age < 18 or has_id == true)
