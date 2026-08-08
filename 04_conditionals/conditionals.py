4e38c53783ee973bfb8dcca0bb77d363994db8da
Sonu V Raikar <omraikar.ai@gmail.com>
2026-08-08 15:32:58 +0530

    Add if elif else examples conditionals.py

--- 04_conditionals/conditionals.py
+++ 04_conditionals/conditionals.py
@@ -5,4 +5,37 @@ if age >=18:
   print("You are an adult.")
 else:
   print("You are not an adult.") 
-  
+
+---
+
+# if, Elif , Else
+
+marks = 75
+
+if marks >= 90:
+  print("Grade A+")
+elif marks >= 75:
+  print("Grade A")
+elif marks >= 60:
+  print("Grade B")
+else:
+  print("Grade C")
+
+
+---
+
+# Student Result
+
+attendance = 85
+
+if attendence >= 75:
+  print("Eligible for exam")
+else:
+  print("Not eligible for exam")
+
+
+if -> first condition
+elif -> another condition if the previous one was false
+else -> everything above was false
+
+---
