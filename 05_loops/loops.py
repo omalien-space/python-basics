eb9c2e6fa3664fd8fd0ce62167669881526c181a
Sonu V Raikar <omraikar.ai@gmail.com>
2026-08-17 14:39:15 +0530

    Add for and while loops examples loops.py

--- 05_loops/loops.py
+++ 05_loops/loops.py
@@ -2,3 +2,34 @@
 
 for number in range (5)
    print (number)
+
+---
+
+# For Loop With Start and Stop
+
+for number in range (1, 6):
+   print (number)
+
+
+# For Loop with Step
+
+for number in range (2, 11 ,2):
+   print(number)
+
+
+# While_Loop
+
+count = 1
+
+While count <= 5:
+    print( count)
+    count += 1
+
+# Star Pattern
+
+stars = 5
+
+while stars > 0:
+   print("*" * stars)
+   stars -= 1
+   
