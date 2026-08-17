191d5c2cc0dc6b4f0a9ef04eff312edb14b68899
Sonu V Raikar <omraikar.ai@gmail.com>
2026-08-17 15:38:01 +0530

    Add functions examples.py

---
+++ 06_functions/functions.py
@@ -0,0 +1,41 @@
+# Python Functions
+
+def greet():
+  print("Hello, Space Explorer!")
+greet() 
+
+---
+
+# Function with Parameters
+
+def greet_person(name):
+  print ("Hello,", name)
+
+greet_person("Omkar")
+
+---
+
+#Addition Function
+
+def add(a, b) :
+  print( a + b)
+
+add(10, 5)
+
+---
+
+# Introduction Function
+
+def introduce(name, college):
+  print(name)
+  print(college)
+
+introduce ("Omkar", "Government First Grade College")
+
+# Space Example
+
+def astronaut(name):
+  print ("Mission:", name)
+
+astronaut ("ISRO")
+astronaut("NASA")
