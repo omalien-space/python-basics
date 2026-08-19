f96404139651852a27434cbc3327e545045062b4
Sonu V Raikar <omraikar.ai@gmail.com>
2026-08-19 14:34:36 +0530

    Add dictionaries examples.py

---
+++ 09_dictionaries/dictionaries.py
@@ -0,0 +1,34 @@
+student = {
+  "name": "Omkar",
+  "collage": "Government First Grade Collage",
+  "cgpa": 8.5,
+  "city" : "Bangalore"
+}
+  print(student)
+
+
+student = {
+  'name': 'Omkar',
+  'collage': 'Government First Grade Collage',
+  'cgpa': 8.5,
+  'city' : 'Bangalore'
+}
+
+---
+
+student = {
+  "name": "Omkar",
+  "college": "Government First Grade College"
+}
+
+student["cgpa"] = 8.5
+print(student)
+
+---
+
+{
+   'name': 'Omkar',
+  'college': 'Government First Grade College'
+}
+
+'cgpa' = 8.5
