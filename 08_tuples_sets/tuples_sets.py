b0bef102afa37c23409cf37c54323ca78a66248f
Sonu V Raikar <omraikar.ai@gmail.com>
2026-08-19 13:31:08 +0530

    Add tuples and sets examples.py

---
+++ 08_tuples_sets/tuples_sets.py
@@ -0,0 +1,55 @@
+# Python Tuples
+
+Planets = ("Mercury", "Venus", "Earth", "Mars")
+print(planets)
+
+('Mercury', 'Venus', 'Earth', 'Mars')
+
+
+---
+
+language = {"Python", "Java", "Phython", "c++"}
+print(languages)
+
+{'Python', 'Java', 'C++'}
+
+---
+
+Tuple -> ()
+set -> {}
+Tuple = immutable (cannot be changed)
+List = mutable (can be changed)
+set = unique values only
+len()
+
+---
+
+# Tuples
+
+missions = ("ISRO", "NASA", "ESA")
+
+print(missions)
+print(missions[2])
+
+# Sets
+
+languages = {"Python", "Java", "Python", "C++"}
+print(language)
+
+language = {'Python, 'java', 'C++'}
+
+            ---
+            
+numbers = {1,2,2,3,4,4,5}
+print(len(numbers))
+
+number = {1,2,3,4,5}
+
+planets = {"Earth", "Mars", "Mars", "Venus"}
+
+print(planets)
+print(len(planets))
+
+planets = {'Earth, 'Mars, 'Venus'}
+planets len = 3
+
