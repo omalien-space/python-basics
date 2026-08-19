c36dba3b9b19342fa85c92c1c9b16a48b8f529e3
Sonu V Raikar <omraikar.ai@gmail.com>
2026-08-19 12:52:38 +0530

    Add Python lists example Lists.py

--- 07_Lists/Lists.py
+++ 07_Lists/Lists.py
@@ -4,4 +4,12 @@ planets = ["Mercury", "Venus", "Earth", "Mars"]
   print(planets)
 
 ['Mercury', 'Venus', earth', 'Mars']
+
+
 ---
+
+colors = ["Red", "Blue", "Green"]
+colors[1] = "Yellow"
+print(colors)
+
+
