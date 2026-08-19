ccab205f88ce15ec2013e04c8981f3ee152cd1f6
Sonu V Raikar <omraikar.ai@gmail.com>
2026-08-19 13:00:58 +0530

    Add Python list examples Lists.py

--- 07_Lists/Lists.py
+++ 07_Lists/Lists.py
@@ -3,7 +3,7 @@
 planets = ["Mercury", "Venus", "Earth", "Mars"]
   print(planets)
 
-['Mercury', 'Venus', earth', 'Mars']
+["Mercury", "Venus", "earth", "Mars"]
 
 
 ---
@@ -11,5 +11,20 @@ planets = ["Mercury", "Venus", "Earth", "Mars"]
 colors = ["Red", "Blue", "Green"]
 colors[1] = "Yellow"
 print(colors)
+
+["Red", "Yellow", "Green"]
+
+
+---
+
+mission = ["ISRO" , "NASA" ]
+missions.append("SpaceX")
+mission[0] = "ESA"
+
+print(missions)
+print(len(missions))
+
+["ESA", "NASA", "SPACEX"]
+ ["3"]
 
 
