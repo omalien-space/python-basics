4a701dcca1e2512161f61f706cf858a4968ab60e
Sonu V Raikar <omraikar.ai@gmail.com>
2026-08-20 15:20:21 +0530

    Add Python string example strings.py

---
+++ 10_strings/strings.py
@@ -0,0 +1,67 @@
+name = "omkar"
+collage = "Government First Grade Collage"
+city = "Bangalore"
+
+--- part 1 ---
+
+name = "omkar"
+print(name)
+
+omkar
+
+---
+
+first_name = "Omkar"
+last_name = "Raikar"
+
+full_name = first_name + " " + last_name
+
+print(full_name)
+
+Omkar Raikar
+
+--- part 2 ---
+
+name = "omkar"
+print(name.upper())
+
+OMKAR
+
+name = "OMKAR"
+print(name.lower())
+
+omkar
+
+--- part 3 ---
+
+word = "Python is amazing"
+print(len(word))
+
+17      ->  ( space is also counted as characters too )
+
+--- part 4 ---
+
+text = "    ISRO    "
+print(text.strip())
+
+ISRO    ->  ( .strip() removes spaces from the beginning and end
+
+--- part 5 ---
+
+message = "I love Java"
+
+message = message.replace("java", "Python")
+print (message)
+
+I love Python
+
+--- Useful string operations i completed ---
+
+.upper()  -> uppercase
+.lower()  -> lowercase
+.strip()  -> remove surrounding spaces
+.replace()-> replace text
+len()     -> count characters
+
+
+
